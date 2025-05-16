5bd7835f3a32a76ff0768b40b9443144bff621db
arqam <arqam.mousa@gmail.com>
2025-05-17 02:26:50 +0300

    Add => transaction codebase
    added transaction for create and update calls in sponsorship.js to make sure that the operations are atomic

--- routes/sponsorship.js
+++ routes/sponsorship.js
@@ -4,11 +4,12 @@ const service = require('../services/sponsorship');
 const {authenticate, authorize} = require('../middleware/auth');
 
 
-router.post('/create', authenticate, authorize("donor"), service.createSponsorship);
 
 router.get('/:id', authenticate, service.getSponsorshipById);
-router.put('/:id/update', authenticate, service.updateSponsorship);
+router.put('/:id', authenticate, service.updateSponsorship);
+router.delete('/:id',authenticate, authorize("donor", "admin"), service.deleteSponsorship);
 
 router.get('/', authenticate, authorize("admin"), service.getSponsorships);
+router.post('/', authenticate, authorize("donor"), service.createSponsorship);
 
 module.exports = router;
