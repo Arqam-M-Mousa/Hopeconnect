9176704d99df9fa3bb10128af3b202593b41c7c9
arqam <arqam.mousa@gmail.com>
2025-05-16 17:33:51 +0300

    Added => initializing server.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const Orphanage = require("./orphanage");
 const OrphanageHelpRequest = require("./orphanageHelpRequest");
 
-Orphanage.hasMany(orphan, { foreignKey: "orphanageId" });
-orphan.belongsTo(Orphanage, { foreignKey: "orphanageId" });
+//Orphanage.hasMany(orphan, { foreignKey: "orphanageId" });
+//orphan.belongsTo(Orphanage, { foreignKey: "orphanageId" });
 
 Orphanage.hasMany(OrphanageHelpRequest, { foreignKey: "orphanageId" });
 OrphanageHelpRequest.belongsTo(Orphanage, { foreignKey: "orphanageId" });
