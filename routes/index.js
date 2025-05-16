e2019f5d0b8d0684696f2740b373beeaba491f79
JADDOU3 <22the7@gmail.com>
2025-05-16 22:57:43 +0300

    Add => sponsorship

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const orphanageRoutes = require('./orphanage');
 const orphanRoutes = require('./orphan');
 const userRoutes = require('./user');
+const SponsorshipRoute = require('./sponsorship');
 
 router.use('/orphanages', orphanageRoutes);
 router.use('/orphan' , orphanRoutes);
 router.use('/user' , userRoutes);
+router.use('/sponsorship' , SponsorshipRoute);
 
 module.exports = router;
---
+++ routes/sponsorship.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const service = require('../services/sponsorship');
+const {authenticate, authorize} = require('../middleware/auth');
+
+
+router.post('/create' , authenticate , authorize("donor"), service.createSponsorship);
+
+router.get('/:id', authenticate , service.getSponsorshipById);
+router.put('/:id/update',authenticate  , service.updateSponsorship);
+
+router.get('/', authenticate ,authorize("admin") ,service.getSponsorships);
+
+module.exports = router;
