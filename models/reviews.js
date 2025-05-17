cabe079773a8f0df90e9d5df601637142524826e
JADDOU3 <22the7@gmail.com>
2025-05-17 21:44:20 +0300

    Adjust => reviews model

--- models/reviews.js
+++ models/reviews.js
@@ -1,43 +1,35 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const review = sequelize.define('reviews', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        userId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'Users',
-                key: 'id'
-            }
-        },
-        orphanId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'Orphans',
-                key: 'id'
-            }
-        },
-        rating: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                min: 1,
-                max: 5
-            }
-        },
-        comment: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-                len: [10, 500]
-            }
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    orphanageId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1,
+            max: 5
         }
+    },
+    comment: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            len: [10, 500]
+        }
+    }
 }, {
     timestamps: true
 });
